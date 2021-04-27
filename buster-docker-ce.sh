ccfc00f7e3e63e158519361566c274064522ebfd
Scott Yuan <yuansc@yahoo.com>
2021-04-27 02:28:46 +0000

    Add ubuntu installation

--- buster-docker-ce.sh
+++ buster-docker-ce.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo here
+apt remove docker docker-engine docker.io containerd runc
+apt install apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+cat <<EOS | tee /etc/apt/sources.list.d/docker.list
+deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu groovy stable
+EOS
+apt update
+apt install docker-ce docker-ce-cli containerd.io
